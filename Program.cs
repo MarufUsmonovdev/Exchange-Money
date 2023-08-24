99f76dee358b32fc3fd4a39a099d3bbb7e1d9cdb
MarufUsmonovdev <142956091+MarufUsmonovdev@users.noreply.github.com>
2023-08-24 17:33:54 +0500

    NEW: About Money

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Exchange money!");
+int sumValue = Convert.ToInt32(Console.ReadLine());
+int anotherValue = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"The exchange money is:{sumValue * anotherValue}");
